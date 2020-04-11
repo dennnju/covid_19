62552200c59e0ed2b0c74e5b4cc21fa4d558aa73
dennnju <dennisnjule@gmail.com>
2020-04-11 17:28:53 +0300

    Update estimator.php

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,37 @@
 <?php
 
 function covid19ImpactEstimator($data)
-{
-  return $data;
-}
+data : {
+	region: {
+		name:"Africa",
+		avarage:19.7,
+		avgDailyIncome:4,
+		avgDailyIncomePopulation:0.73
+	}
+	periodType:"days",
+	timeToElapse:38,
+	reportedCases:2747,
+	population:92931687,
+	totallHospitalBeds:678874
+		}
+		estimate: {
+			impact: {
+				currentlyInfected:27470,
+				infectionsByRequestedTime:112517120,
+				severeCasesByRequestedTime:16877568,
+				hospitalBedsByRequestedTime:-16639962,
+				casesForICUByRequestedTime:5625856,
+				casesForVentilatorsByRequestedTime:2250342,
+				dollarsInFlight:12484899635.2
+			},
+	severeImpact: {
+		currentlyInfected:137350,
+		infectionsByRequestedTime:562585600,
+		severeCasesByRequestedTime:84387840,
+		hospitalBedsByRequestedTime:-84150234,
+		casesForICUByRequestedTime:28129280,
+		casesForVentilatorsByRequestedTime:11251712,
+		dollarsInFlight:62424498176
+	}
+		}
+  
